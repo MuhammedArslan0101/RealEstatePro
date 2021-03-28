c719795c6efc971944983aa4ca87619521ff45a7
Muhammed ARSLAN <=md.arslan0101@gmail.com>
2021-03-29 01:07:43 +0300

    Register

---
+++ RealEstatePro/Controllers/AccountController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using RealEstatePro.Identity;
+using RealEstatePro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RealEstatePro.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<ApplicationUser> UserManager;
+        private RoleManager<ApplicationRole> RoleManager;
+        public AccountController()
+        {
+            var userStore = new UserStore<ApplicationUser>(new IdentityDataContext());
+            UserManager = new UserManager<ApplicationUser>(userStore);
+            var roleStore = new RoleStore<ApplicationRole>(new IdentityDataContext());
+            RoleManager = new RoleManager<ApplicationRole>(roleStore);
+
+
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(Register model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser();
+                user.Name = model.Name;
+                user.UserName = model.Username;
+                user.Surname = model.Surname;
+                user.Email = model.Email;
+                var result = UserManager.Create(user, model.Password);
+                if (result.Succeeded)
+                {
+                    if (RoleManager.RoleExists("user"))
+                    {
+
+                        UserManager.AddToRole(user.Id, "user");
+                    }
+                    return RedirectToAction("Login", "Account");   
+                }else
+                {
+                    ModelState.AddModelError("RegisterUserError", "Kullanıccı oluşturma hatası");
+                }
+            }
+            return View(model);
+        }
+
+
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RealEstatePro/Models/Register.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RealEstatePro.Models
+{
+    public class Register
+    {
+        [Required]
+        [DisplayName("Adı")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter student name.")]
+        [DisplayName("Soyadı")]
+        public string Surname { get; set; }
+        [Required]
+        [DisplayName("Email")]
+        [EmailAddress(ErrorMessage ="Geçersiz Email adresi")]
+        public string Email { get; set; }
+        [Required]
+        [DisplayName("KullanıcıAdı")]
+        public string Username { get; set; }
+        [Required]
+        [DisplayName("Şifre")]
+        public string Password { get; set; }
+        [Required]
+        [DisplayName("Şifre Tekrar")]
+        [Compare("Password" ,ErrorMessage ="Şifreler Aynı Değil")]
+        public string RePassword { get; set; }
+
+    }
+}
