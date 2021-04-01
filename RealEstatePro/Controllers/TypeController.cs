37d99e9c894cb0428fc13a79b182ab08b29ca755
Muhammed ARSLAN <=md.arslan0101@gmail.com>
2021-04-01 23:36:03 +0300

    Type Of Status ( House , Market , .....) Crud Operations

---
+++ RealEstatePro/Controllers/TypeController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RealEstatePro.Models;
+using Type = RealEstatePro.Models.Type;
+
+namespace RealEstatePro.Controllers
+{
+    public class TypeController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        // GET: Type
+        public ActionResult Index()
+        {
+            var types = db.Types.Include(t => t.Status);
+            return View(types.ToList());
+        }
+
+        // GET: Type/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Type type = db.Types.Find(id);
+            if (type == null)
+            {
+                return HttpNotFound();
+            }
+            return View(type);
+        }
+
+        // GET: Type/Create
+        public ActionResult Create()
+        {
+            ViewBag.StatusId = new SelectList(db.Statuses, "StatusId", "StatusName");
+            return View();
+        }
+
+        // POST: Type/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TypeId,TypeName,StatusId")] Type type)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Types.Add(type);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.StatusId = new SelectList(db.Statuses, "StatusId", "StatusName", type.StatusId);
+            return View(type);
+        }
+
+        // GET: Type/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Type type = db.Types.Find(id);
+            if (type == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.StatusId = new SelectList(db.Statuses, "StatusId", "StatusName", type.StatusId);
+            return View(type);
+        }
+
+        // POST: Type/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TypeId,TypeName,StatusId")] Type type)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(type).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.StatusId = new SelectList(db.Statuses, "StatusId", "StatusName", type.StatusId);
+            return View(type);
+        }
+
+        // GET: Type/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Type type = db.Types.Find(id);
+            if (type == null)
+            {
+                return HttpNotFound();
+            }
+            return View(type);
+        }
+
+        // POST: Type/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Type type = db.Types.Find(id);
+            db.Types.Remove(type);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
