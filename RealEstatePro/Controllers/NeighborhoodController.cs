6b6965bcb8d98562f66ae5af00fe0ab9a6489606
Muhammed ARSLAN <=md.arslan0101@gmail.com>
2021-04-01 18:08:09 +0300

    Neighborhood Crud Operations

---
+++ RealEstatePro/Controllers/NeighborhoodController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RealEstatePro.Models;
+
+namespace RealEstatePro.Controllers
+{
+    public class NeighborhoodController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        // GET: Neighborhood
+        public ActionResult Index()
+        {
+            var neighborhoods = db.Neighborhoods.Include(n => n.District);
+            return View(neighborhoods.ToList());
+        }
+
+        // GET: Neighborhood/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Neighborhood neighborhood = db.Neighborhoods.Find(id);
+            if (neighborhood == null)
+            {
+                return HttpNotFound();
+            }
+            return View(neighborhood);
+        }
+
+        // GET: Neighborhood/Create
+        public ActionResult Create()
+        {
+            ViewBag.DistrictId = new SelectList(db.Districts, "DistrictId", "DistrictName");
+            return View();
+        }
+
+        // POST: Neighborhood/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "NeighborhoodId,NeighborhoodName,DistrictId")] Neighborhood neighborhood)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Neighborhoods.Add(neighborhood);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DistrictId = new SelectList(db.Districts, "DistrictId", "DistrictName", neighborhood.DistrictId);
+            return View(neighborhood);
+        }
+
+        // GET: Neighborhood/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Neighborhood neighborhood = db.Neighborhoods.Find(id);
+            if (neighborhood == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DistrictId = new SelectList(db.Districts, "DistrictId", "DistrictName", neighborhood.DistrictId);
+            return View(neighborhood);
+        }
+
+        // POST: Neighborhood/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "NeighborhoodId,NeighborhoodName,DistrictId")] Neighborhood neighborhood)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(neighborhood).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DistrictId = new SelectList(db.Districts, "DistrictId", "DistrictName", neighborhood.DistrictId);
+            return View(neighborhood);
+        }
+
+        // GET: Neighborhood/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Neighborhood neighborhood = db.Neighborhoods.Find(id);
+            if (neighborhood == null)
+            {
+                return HttpNotFound();
+            }
+            return View(neighborhood);
+        }
+
+        // POST: Neighborhood/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Neighborhood neighborhood = db.Neighborhoods.Find(id);
+            db.Neighborhoods.Remove(neighborhood);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
