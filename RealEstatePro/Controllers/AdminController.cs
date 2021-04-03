f3253b44a6e54536fe28f90c98e2c43c00705630
Muhammed ARSLAN <=md.arslan0101@gmail.com>
2021-04-03 22:11:05 +0300

    Admin Panel

---
+++ RealEstatePro/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using RealEstatePro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+namespace RealEstatePro.Controllers
+{
+    public class AdminController : Controller
+    {
+        DataContext db = new DataContext();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+      
+    }
+}
